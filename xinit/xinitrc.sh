8f731f05866bf061392c3b8484bf39007b285c5a
geekymoose <constantin.masson@umontreal.ca>
2018-06-15 12:26:24 -0400

    Update i3 config

--- xinit/xinitrc.sh
+++ xinit/xinitrc.sh
@@ -22,6 +22,7 @@ if [ -d /etc/X11/xinit/xinitrc.d ] ; then
 fi
 
 # Special setting for my Nvidia GPU, but shouldn't be required by others
+# See https://wiki.archlinux.org/index.php/NVIDIA_Optimus
 xrandr --setprovideroutputsource modesetting NVIDIA-0
 xrandr --auto
 
