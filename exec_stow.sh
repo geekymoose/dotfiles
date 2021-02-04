ba62bbf961b0724c0094d1528c35a5df81e20d72
geekymoose <constantin.masson@umontreal.ca>
2021-02-04 19:24:00 +0100

    Update stow to create only file symlinks (use --no-folding)

--- exec_stow.sh
+++ exec_stow.sh
@@ -2,5 +2,5 @@
 
 for file in ./src/*; do
     echo -e "\e[34m---> Stowing package ${file##*/}\e[0m"
-    stow --verbose=1 --dir="./src" --target="$HOME" ${file##*/}
+    stow --verbose=1 --no-folding --dir="./src" --target="$HOME" ${file##*/}
 done
