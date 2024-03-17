29cc07b9e3ff054e008fefbc1e00d5b6cb324156
geekymoose <constantin.masson@umontreal.ca>
2024-03-17 20:29:40 +0100

    Add man colors and some i3 default programs

--- src/sh/.config/sh/environments.sh
+++ src/sh/.config/sh/environments.sh
@@ -10,7 +10,12 @@ export XDG_DATA_HOME="$HOME/.local/share"
 
 export EDITOR=nvim
 export VISUAL=nvim 
-export PAGER=bat
+
 export TERMINAL=alacritty
 export TERM=alacritty
 export BROWSER=firefox
+
+# Color in man page
+# See https://wiki.archlinux.org/title/Color_output_in_console#man
+export MANPAGER="less -R --use-color -Dd+r -Du+b"
+export MANROFFOPT="-P -c"
