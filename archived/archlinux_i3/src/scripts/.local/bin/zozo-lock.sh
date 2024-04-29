f94ea5a6ae56b1750ebef7ddcecf178a880136fa
geekymoose <constantin.masson@umontreal.ca>
2024-04-29 22:27:31 +0200

    Remove the old archived config (not used anymore)

--- archived/archlinux_i3/src/scripts/.local/bin/zozo-lock.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-i3lock -f -e -c 351d4c
--- archived/archlinux_i3/src/scripts/.local/bin/zozo-start-xorg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Start Xorg server according to logged terminal
-if [ -z "$DISPLAY" ] && [ -n "$XDG_VTNR" ]; then
-    case $XDG_VTNR in
-        1) startx ~/.xinitrc i3 ;;
-        *) echo " * No xinit environment set for tty$XDG_VTNR";;
-    esac
-fi
