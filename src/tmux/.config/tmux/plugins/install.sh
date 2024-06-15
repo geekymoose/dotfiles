642c90576503fb143ce6f83a08fc7103dc7619b4
geekymoose <constantin.masson@umontreal.ca>
2024-06-15 22:02:20 +0200

    Remove the tmux plugins (not used for now, so to make the config simplier)

--- src/tmux/.config/tmux/plugins/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Run this script to clone the TMux plugin manager
-
-if [[ -e tpm && -d tpm ]]; then
-    echo "TPM plugin is already installed"
-else
-    git clone 'https://github.com/tmux-plugins/tpm'
-fi
-
