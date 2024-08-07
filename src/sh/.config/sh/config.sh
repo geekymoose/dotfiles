aade5fd3699f7948d0ea57ef9fcaa55e83a2011c
geekymoose <constantin.masson@umontreal.ca>
2024-08-07 16:04:43 +0200

    Add several helper bash methods in the sh config

---
+++ src/sh/.config/sh/config.sh
@@ -0,0 +1,30 @@
+# ------------------------------------------------------------------------------
+# Set of custom configuration
+# ------------------------------------------------------------------------------
+
+
+umask 027 # By default, no perm for 'a' group (file: 640, folder: 750)
+
+
+# SSH
+# Make sure that only one ssh-agent process runs at a time
+# See https://wiki.archlinux.org/index.php/ssh_keys (section ssh-agent)
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    ssh-agent > "$XDG_RUNTIME_DIR/ssh-agent.env"
+fi
+if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
+    source "$XDG_RUNTIME_DIR/ssh-agent.env" > /dev/null
+fi
+
+
+# RANGER
+# Preventing nested ranger instances
+# See https://wiki.archlinux.org/index.php/Ranger
+function ranger() {
+    if [ -z "$RANGER_LEVEL" ]; then
+        /usr/bin/ranger "$@"
+    else
+        exit
+    fi
+}
+
--- src/sh/.config/sh/utils.sh
+++ src/sh/.config/sh/utils.sh
@@ -1,29 +1,50 @@
 # ------------------------------------------------------------------------------
-# Miscs utilities
+# Set of reusable utilities
+# These are targetted to be used in shell scripts
 # ------------------------------------------------------------------------------
 
-umask 027 # By default, no perm for 'a' group (file: 640, folder: 750)
+export COLOR_ERROR='\e[31m'
+export COLOR_SUCCESS='\033[32m'
+export COLOR_CMD='\e[34m'
+export COLOR_NORMAL='\e[0m'
 
+# Execute the provided command, but prints the full cmd before executing it.
+# Param 1: the command line to execute
+function apply_cmd() {
+    echo -e "${COLOR_CMD}${1}${COLOR_NORMAL}"
+    eval $1
+}
 
-# SSH
-# Make sure that only one ssh-agent process runs at a time
-# See https://wiki.archlinux.org/index.php/ssh_keys (section ssh-agent)
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent > "$XDG_RUNTIME_DIR/ssh-agent.env"
-fi
-if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
-    source "$XDG_RUNTIME_DIR/ssh-agent.env" > /dev/null
-fi
+# Checks whether the provided command is installed
+# Exit if is not installed
+# Param 1: the command
+function ensure_is_installed {
+    cmdname="$1"
+    which "$cmdname" >> /dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        echo -e "${COLOR_ERROR}$cmdname not found${COLOR_NORMAL}"
+        exit 42
+    fi
+}
 
+# Checks whether the current hostname is equal to the expected one
+# Exit if is not the expected value
+# Param 1: the expected hostname
+function ensure_on_hostname {
+    expected_name="$1"
+    if [[ $(hostname) != "$expected_name" ]]; then
+        echo -e "${COLOR_ERROR}Action not allowed on this computer (only usable on ${expected_name})${COLOR_NORMAL}"
+        exit 42
+    fi
+}
 
-# RANGER
-# Preventing nested ranger instances
-# See https://wiki.archlinux.org/index.php/Ranger
-function ranger() {
-    if [ -z "$RANGER_LEVEL" ]; then
-        /usr/bin/ranger "$@"
-    else
-        exit
+# Check key is plugged
+function ensure_disklabel_exists {
+    disk_label="$1"
+    disk_exists=$(ls /dev/disk/by-label/ | grep -i $disk_label)
+    if [ -z "${disk_exists}" ]; then
+        echo -e "${COLOR_ERROR}[ERR] The partition with label \"$disk_label\" does not exists (make sure it is mounted)${COLOR_NORMAL}"
+        exit 42
     fi
 }
 
