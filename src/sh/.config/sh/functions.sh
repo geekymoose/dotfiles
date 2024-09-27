9d8eb2ac6817b7d2322d3100c7331b68521102e4
geekymoose <constantin.masson@umontreal.ca>
2024-09-27 12:28:42 +0200

    Add several methods in the functions.sh bash utilities

--- src/sh/.config/sh/functions.sh
+++ src/sh/.config/sh/functions.sh
@@ -1,28 +1,51 @@
 # ------------------------------------------------------------------------------
-# Set of reusable utilities
-# These are targetted to be used in shell scripts
+# Set of reusable functions
+# These are mostly targetted to be used in my personal scripts
+# To include in script: source "${HOME}/.config/sh/functions.sh"
 # ------------------------------------------------------------------------------
 
-export COLOR_ERROR='\e[31m'
-export COLOR_SUCCESS='\033[32m'
-export COLOR_CMD='\e[34m'
-export COLOR_NORMAL='\e[0m'
 
-# Execute the provided command, but prints the full cmd before executing it.
+COLOR_ERROR='\e[31m'
+COLOR_SUCCESS='\033[32m'
+COLOR_CMD='\e[34m'
+COLOR_NORMAL='\e[0m'
+
+
+# Executes the provided command, but prints the full cmd before executing it.
+# For CI script, you may use `set -v` instead
 # Param 1: the command line to execute
 function apply_cmd() {
     echo -e "${COLOR_CMD}${1}${COLOR_NORMAL}"
     eval $1
 }
 
+# Returns a formatted version of date now (e.g., 2024-09-27_115031)
+function formatted_date_now() {
+    echo $(date +%F_%H%M%S)
+}
+
+# Checks whether the provided address is reachable
+# Address should be fast reachable (sort timeout)
+# Exit if not reachable
+# Param 1: IP Address
+function ensure_is_addr_reachable() {
+    local addr="$1"
+    echo "Trying to reach ${addr}..."
+    ping -Dc 1 -w 1 $addr >> /dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        echo -e "${COLOR_ERROR}[ERR] Unable to reach the requested address ${addr}${COLOR_NORMAL}"
+        exit 42
+    fi
+}
+
 # Checks whether the provided command is installed
 # Exit if is not installed
 # Param 1: the command
 function ensure_is_installed {
-    cmdname="$1"
+    local cmdname="$1"
     which "$cmdname" >> /dev/null 2>&1
     if [[ $? -ne 0 ]]; then
-        echo -e "${COLOR_ERROR}$cmdname not found${COLOR_NORMAL}"
+        echo -e "${COLOR_ERROR}[ERR] Command \"$cmdname\" not found${COLOR_NORMAL}"
         exit 42
     fi
 }
@@ -31,20 +54,67 @@ function ensure_is_installed {
 # Exit if is not the expected value
 # Param 1: the expected hostname
 function ensure_on_hostname {
-    expected_name="$1"
+    local expected_name="$1"
     if [[ $(hostname) != "$expected_name" ]]; then
-        echo -e "${COLOR_ERROR}Action not allowed on this computer (only usable on ${expected_name})${COLOR_NORMAL}"
+        echo -e "${COLOR_ERROR}[ERR] Action not allowed on this computer (only usable on ${expected_name})${COLOR_NORMAL}"
         exit 42
     fi
 }
 
-# Check key is plugged
+# Checks if key with provided label is plugged
+# Exit if not plugged
 function ensure_disklabel_exists {
-    disk_label="$1"
-    disk_exists=$(ls /dev/disk/by-label/ | grep -i $disk_label)
-    if [ -z "${disk_exists}" ]; then
+    local disk_label="$1"
+    local disk_exists=$(ls /dev/disk/by-label/ 2> /dev/null | grep -i $disk_label 2> /dev/null )
+    if [[ -z "${disk_exists}" ]]; then
         echo -e "${COLOR_ERROR}[ERR] The partition with label \"$disk_label\" does not exists (make sure it is mounted)${COLOR_NORMAL}"
         exit 42
     fi
+}
+
+# Checks if the folder exists
+# Exit if doesn't exists
+# Param 1: the folder full path
+function ensure_folder_exists {
+    local folderpath="$1"
+    if ! [[ -d "$folderpath" ]]; then
+        echo -e "${COLOR_ERROR}[ERR] The folder \"${folderpath}\" does not exists or is not a valid directory${COLOR_NORMAL}"
+        exit 42
+    fi
+}
+
+# Runs rsync backup (checks that src and dest are valid folders)
+# Exist if invalid parameters
+# Param 1: source folder
+# Param 2: dest folder
+function run_rsync_backup() {
+    local src="$1"
+    local dst="$2"
+    ensure_folder_exists ${src}
+    ensure_folder_exists ${dst}
+    ensure_is_installed "rsync"
+
+    echo -e "${COLOR_CMD}Running RSync backup..."
+    echo -e "${COLOR_CMD}Source: ${src}"
+    echo -e "${COLOR_CMD}Destination: ${dst}${COLOR_NORMAL}"
+
+    # Run rsync and make sure we add prefix "/" (see documentation)
+    apply_cmd "rsync -avr --delete ${src}/ ${dst}/"
+}
+
+# Runs adb-sync backup (checks that src and dest are valid folders)
+# Exist if invalid parameters
+# Param 1: source folder
+# Param 2: dest folder
+function run_adb_sync_backup() {
+    local src="$1"
+    local dst="$2"
+    ensure_folder_exists ${src}
+    ensure_folder_exists ${dst}
+    ensure_is_installed "adb"
+    ensure_is_installed "adb-sync"
+
+    apply_cmd "adb devices"
+    apply_cmd "adb-sync --delete ${SRC}/ ${DST}/"
 }
 
