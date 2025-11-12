8e49d50702457403145ed0afb9ad7db69d506d5f
geekymoose <constantin.masson@umontreal.ca>
2025-11-12 02:56:22 +0100

    Update the bash environament variables

--- src/sh/.config/sh/environments.sh
+++ src/sh/.config/sh/environments.sh
@@ -2,26 +2,25 @@
 # Environments
 # ------------------------------------------------------------------------------
 
+# Path
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$PATH:$HOME/.lmstudio/bin"
-
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 
+# Applications
 export EDITOR=nvim
 export VISUAL=nvim
-
 export TERMINAL=alacritty
 export TERM=alacritty
 export BROWSER=firefox
 
-# Custom variables
-export GODOT_PATH=$(which godot)
-
-# Color in man page (See https://wiki.archlinux.org/title/color_output_in_console#man)
+# Color in man page
+# See https://wiki.archlinux.org/title/color_output_in_console#man
 export MANPAGER="less -R --use-color -Dd+r -Du+b"
 export MANROFFOPT="-P -c"
 
-# QT config for all environments (see https://wiki.archlinux.org/title/qt#gtk_and_qt)
+# QT config for all environments
+# See https://wiki.archlinux.org/title/qt#gtk_and_qt
 export QT_QPA_PLATFORMTHEME=qt6ct
