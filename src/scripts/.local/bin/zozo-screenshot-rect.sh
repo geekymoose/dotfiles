c1f40c423eb92f181ff3dae02e6a217ec0182c10
geekymoose <constantin.masson@umontreal.ca>
2025-11-15 16:03:10 +0100

    Refine the sway configuration to include the zozo scripts (and remove these deprecated scripts)

--- src/scripts/.local/bin/zozo-screenshot-rect.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Run the screenshot utility (open for a specific rectangle)
-
-if [[ $XDG_SESSION_TYPE == "wayland" ]];then
-    slurp | grim -g -
-else
-    xfce4-screenshooter -w -s "$HOME/downloads/"
-fi
--- src/scripts/.local/bin/zozo-screenshot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Run the screenshot utility
-
-if [[ $XDG_SESSION_TYPE == "wayland" ]];then
-    grim
-else
-    xfce4-screenshooter
-fi
